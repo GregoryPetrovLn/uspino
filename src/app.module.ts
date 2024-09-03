f98c31d42208079c2ad443b2cc56a86d87a450a9
GregoryPetrovLn <grishap96123@gmail.com>
2024-09-03 12:39:14 +0300

    init project

---
+++ src/app.module.ts
@@ -0,0 +1,51 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ThrottlerModule } from '@nestjs/throttler';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { CacheModule } from './cache/cache.module';
+import { EmailModule } from './email/email.module';
+import { RabbitMQModule } from './rabbitmq/rabbitmq.module';
+import { UsersModule } from './users/users.module';
+import { WeatherModule } from './weather/weather.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('POSTGRES_HOST'),
+        port: +configService.get<number>('POSTGRES_PORT'),
+        username: configService.get('POSTGRES_USER'),
+        password: configService.get('POSTGRES_PASSWORD'),
+        database: configService.get('POSTGRES_DB'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: configService.get('NODE_ENV') !== 'production',
+      }),
+      inject: [ConfigService],
+    }),
+    ThrottlerModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ([{
+        ttl: config.get('RATE_LIMIT_TTL'),
+        limit: config.get('RATE_LIMIT_MAX'),
+      }]),
+    }),
+    AuthModule,
+    UsersModule,
+    WeatherModule,
+    CacheModule,
+    RabbitMQModule,
+    EmailModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
