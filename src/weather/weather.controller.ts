9471068185839d67fd2c88ef9a7ebf61041f46d0
GregoryPetrovLn <grishap96123@gmail.com>
2024-09-03 16:54:52 +0300

    removed unnesesary

--- src/weather/weather.controller.ts
+++ src/weather/weather.controller.ts
@@ -1,47 +1,26 @@
-import {
-  Controller,
-  Get,
-  Query,
-  Req,
-  UseGuards
-} from '@nestjs/common';
+import { Controller, Get, Query, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { Throttle } from '@nestjs/throttler';
-import { Request } from 'express';
-import { JwtStrategy } from 'src/auth/jwt.strategy';
 import { WeatherService } from './weather.service';
 
 @Controller('weather')
 @UseGuards(AuthGuard('jwt'))
 export class WeatherController {
-  constructor(
-    private weatherService: WeatherService,
-    private jwtStrategy: JwtStrategy,
-  ) {}
+  constructor(private weatherService: WeatherService) {}
   @Get()
   @Throttle({ default: { limit: 5, ttl: 60000 } })
-  async getWeather(
-    @Query('city') city: string,
-
-    @Req() request: Request,
-  ) {
-  
+  async getWeather(@Query('city') city: string) {
     return this.weatherService.getWeather(city);
   }
 
   @Get('limit')
-  async getLimit(@Req() request: Request) {
-    console.log('Auth Header:', request.headers.authorization);
-    console.log('User:', request.user);
+  async getLimit() {
     return this.weatherService.getUserLimit();
   }
 
   @Get('test')
   @UseGuards(AuthGuard('jwt'))
-  async testEndpoint(@Req() request: Request) {
-    console.log('Weather test endpoint accessed');
-    console.log('Auth Header:', request.headers.authorization);
-    console.log('User:', request['user']);
+  async testEndpoint() {
     return { message: 'Weather test endpoint is working' };
   }
 }
