78e9c48165f393624fdcee6b6c443eed32f87af3
GregoryPetrovLn <grishap96123@gmail.com>
2024-09-04 23:28:09 +0300

    fixed error request

---
+++ src/guards/user-throttler.guard.ts
@@ -0,0 +1,26 @@
+import { ExecutionContext, Injectable } from '@nestjs/common';
+import { ThrottlerGuard } from '@nestjs/throttler';
+
+@Injectable()
+export class UserThrottlerGuard extends ThrottlerGuard {
+  protected async getTracker(req: Record<string, any>): Promise<string> {
+    const user = req.user;
+    return user ? user.id.toString() : super.getTracker(req);
+  }
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const res = context.switchToHttp().getResponse();
+
+    // Check if res.header is a function, if not, provide a fallback
+    if (typeof res.header !== 'function') {
+      res.header = (name: string, value: string) => {
+        if (!res.headers) {
+          res.headers = {};
+        }
+        res.headers[name] = value;
+      };
+    }
+
+    return super.canActivate(context);
+  }
+}
